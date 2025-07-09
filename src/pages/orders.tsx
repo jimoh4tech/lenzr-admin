f5f5e8f8ea3b0b503324a5f5a985e9d13a18b747
Jimoh4Tech <olamide14044@gmail.com>
2025-07-09 16:10:50 +0100

    feat(order): add new details

--- src/pages/orders.tsx
+++ src/pages/orders.tsx
@@ -1,9 +1,15 @@
+import { OrdersTable } from "@/components/custom/orders-table";
 import { Stack, Text } from "@chakra-ui/react";
 
 export const Orders = () => {
   return (
     <Stack gap={4} p={5} bgColor={"gray.50"} borderRadius={"md"} minH={"87vh"}>
-      <Text>This is Orders</Text>
+      <Stack gap={4} bgColor={"white"} p={5} borderRadius={"md"}>
+        <Text fontSize="xl" fontWeight="bold">
+          Orders
+        </Text>
+        <OrdersTable />
+      </Stack>
     </Stack>
   );
 };
--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,9 +1,15 @@
+import { UserTable } from "@/components/custom/user-table";
 import { Stack, Text } from "@chakra-ui/react";
 
 export const Users = () => {
   return (
     <Stack gap={4} p={5} bgColor={"gray.50"} borderRadius={"md"} minH={"87vh"}>
-      <Text>This is users</Text>
+      <Stack gap={4} bgColor={"white"} p={5} borderRadius={"md"}>
+        <Text fontSize="xl" fontWeight="bold">
+          Users List
+        </Text>
+        <UserTable />
+      </Stack>
     </Stack>
   );
 };
